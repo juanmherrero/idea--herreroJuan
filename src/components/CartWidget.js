1a835aabf6152df63c199f23342315045a66c336
juanmherrero <105325161+juanmherrero@users.noreply.github.com>
2022-10-04 11:27:45 -0300

    correcciones

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -2,15 +2,21 @@ import carro from "../img/carrito.png"
 import "../App.css"
 import { useContext, useEffect } from "react";
 import { CartContext } from "./CartContext";
+import { MDBBadge, MDBIcon } from 'mdb-react-ui-kit';
 
 const Carro = () => {
     const ctx = useContext(CartContext);
 
     return (
-        <div id="divCarro" {...ctx.calcItemQty()}>
+        <div id="divCarro" >
             <img id="carro" src={carro} alt=""></img>
+            <MDBIcon fas icon='envelope' size='lg' />
+            <MDBBadge color='danger' notification pill>
+                {ctx.calcItemQty()}
+            </MDBBadge>            
         </div>
     );
 }
+
 
 export default Carro;
